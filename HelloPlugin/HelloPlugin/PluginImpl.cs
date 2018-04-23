10dc5e984d40ed2fd6aea20085fbf4e031e95341
unknown <zefengsong@gwmail.gwu.edu>
2018-04-23 17:03:18 -0400

    Add HelloPlugin

---
+++ HelloPlugin/HelloPlugin/PluginImpl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using pGina.Shared.Types;
+using log4net;
+
+namespace pGina.Plugin.HelloPlugin
+{
+    public class PluginImpl : pGina.Shared.Interfaces.IPluginAuthentication
+    {
+
+        public string Name
+        {
+            get { return "Hello"; }
+        }
+
+        public string Description
+        {
+            get { return "Authenticates all users with 'hello' in the username and 'pGina' in the password"; }
+        }
+
+        private static readonly Guid m_uuid = new Guid("E70D5C46-74AA-4906-B0D1-658989C4A580");
+
+        public Guid Uuid
+        {
+            get { return m_uuid; }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public void Starting() { }
+
+        public void Stopping() { }
+
+        public BooleanResult AuthenticateUser(SessionProperties properties)
+        {
+            UserInformation userInfo = properties.GetTrackedSingle<UserInformation>();
+
+            if (userInfo.Username.Contains("hello") && userInfo.Password.Contains("pGina"))
+            {
+                // Successful authentication
+                return new BooleanResult() { Success = true };
+            }
+            // Authentication failure
+            return new BooleanResult() { Success = false, Message = "Incorrect username or password." };
+        }
+    }
+}
